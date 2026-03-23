0799ee08c0d890f4485abbedfc4b5e594d803e5a
enjxyer <hfgtylop@gmail.com>
2026-03-23 16:35:24 +0500

    feat: add rate limiter for my api, add gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Fista6k/Url-Shorterer.git/internal/adapter"
 	controller "github.com/Fista6k/Url-Shorterer.git/internal/controller/http"
@@ -19,7 +24,8 @@ func init() {
 }
 
 func main() {
-
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 	port := flag.String("port", "8080", "where i run server")
 	flag.Parse()
 
@@ -33,7 +39,27 @@ func main() {
 	service := service.NewShortererService(storage)
 	r := controller.NewRouter(service)
 	addr := ":" + *port
-	if err = r.Router.Run(addr); err != nil {
-		log.Fatal(err)
+
+	server := &http.Server{
+		Handler: r.Router,
+		Addr:    addr,
 	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C to force")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
--- internal/controller/http/router.go
+++ internal/controller/http/router.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"net/http"
+	"sync"
+
 	"github.com/Fista6k/Url-Shorterer.git/internal/service"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
 )
 
 type Router struct {
@@ -22,7 +26,38 @@ func NewRouter(service *service.ShortererService) *Router {
 }
 
 func (r Router) AddEndPoints(service *service.ShortererService) {
+	r.Router.Use(r.RateLimiter())
 	r.Router.GET("/", service.MainPage)
 	r.Router.POST("/shorten", service.Shorten)
 	r.Router.GET("/:shortUrl", service.Redirect)
+}
+
+func (r Router) RateLimiter() gin.HandlerFunc {
+	type client struct {
+		limiter *rate.Limiter
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+
+		mu.Lock()
+		if _, ok := clients[ip]; !ok {
+			clients[ip] = &client{limiter: rate.NewLimiter(10, 20)}
+		}
+		cl := clients[ip]
+		mu.Unlock()
+
+		if !cl.limiter.Allow() {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error": "rate limit exceeded",
+			})
+		}
+
+		c.Next()
+	}
 }
